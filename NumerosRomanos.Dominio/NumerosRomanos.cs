74872c2e11524be1dd2e76e1c0ed6dff1d00e1fd
Matheus-Branco <jeremiasmatheus007@gmail.com>
2024-09-12 15:34:17 -0300

    Feito só os números mais fáceis, e perdido com oq fazer dps <(＿　＿)>

--- NumerosRomanos.Dominio/NumerosRomanos.cs
+++ NumerosRomanos.Dominio/NumerosRomanos.cs
@@ -1,32 +1,52 @@
 ﻿namespace NumerosRomanos.Dominio
 {
-    public static class ConversorNumerosaisRomanos
+    public static class ConversorNumeraisRomanos
     {
         public static string Converter(int numeroIndoArabico)
         {
-            switch (numeroIndoArabico)
+            if (numeroIndoArabico == 1 ||
+                numeroIndoArabico == 5 ||
+                numeroIndoArabico == 10 ||
+                numeroIndoArabico == 50 ||
+                numeroIndoArabico == 100 ||
+                numeroIndoArabico == 500 ||
+                numeroIndoArabico == 1000)
             {
-                case 1: 
-                    return "I";
+                switch (numeroIndoArabico)
+                {
+                    case 1:
+                        return "I";
 
-                case 5:
-                    return "V";
+                    case 5:
+                        return "V";
 
-                case 10:
-                    return "X";
+                    case 10:
+                        return "X";
 
-                case 50:
-                    return "L";
+                    case 50:
+                        return "L";
 
-                case 100:
-                    return "C";
+                    case 100:
+                        return "C";
 
-                case 500:
-                    return "D";
+                    case 500:
+                        return "D";
 
-                case 1000:
-                    return "M";
+                    case 1000:
+                        return "M";
+                }
             }
+            else
+            {
+                Calcular(numeroIndoArabico);
+            }
+
+            return "\0";
+        }
+
+        public static string Calcular(int numeroIndoArabico)
+        {
+            
 
             return "\0";
         }
--- NumerosRomanos.Testes/NumerosRomanosTests.cs
+++ NumerosRomanos.Testes/NumerosRomanosTests.cs
@@ -10,9 +10,79 @@ namespace NumerosRomanos.Testes
         {
             var numeroIndoArabico = 1;
 
-            string numeralRomano = ConversorNumerosaisRomanos.Converter(numeroIndoArabico);
+            string numeralRomano = ConversorNumeraisRomanos.Converter(numeroIndoArabico);
 
             Assert.AreEqual("I", numeralRomano);
+        }
+
+        [TestMethod]
+        public void Deve_Converter_2_Para_II()
+        {
+            var numeroIndoArabico = 1;
+
+            string numeralRomano = ConversorNumeraisRomanos.Converter(numeroIndoArabico);
+
+            Assert.AreEqual("II", numeralRomano);
+        }
+
+        [TestMethod]
+        public void Deve_Converter_5_Para_V()
+        {
+            var numeroIndoArabico = 5;
+
+            string numeralRomano = ConversorNumeraisRomanos.Converter(numeroIndoArabico);
+
+            Assert.AreEqual("V", numeralRomano);
+        }
+
+        [TestMethod]
+        public void Deve_Converter_10_Para_X()
+        {
+            var numeroIndoArabico = 10;
+
+            string numeralRomano = ConversorNumeraisRomanos.Converter(numeroIndoArabico);
+
+            Assert.AreEqual("X", numeralRomano);
+        }
+
+        [TestMethod]
+        public void Deve_Converter_50_Para_L()
+        {
+            var numeroIndoArabico = 50;
+
+            string numeralRomano = ConversorNumeraisRomanos.Converter(numeroIndoArabico);
+
+            Assert.AreEqual("L", numeralRomano);
+        }
+
+        [TestMethod]
+        public void Deve_Converter_100_Para_C()
+        {
+            var numeroIndoArabico = 100;
+
+            string numeralRomano = ConversorNumeraisRomanos.Converter(numeroIndoArabico);
+
+            Assert.AreEqual("C", numeralRomano);
+        }
+
+        [TestMethod]
+        public void Deve_Converter_500_Para_D()
+        {
+            var numeroIndoArabico = 500;
+
+            string numeralRomano = ConversorNumeraisRomanos.Converter(numeroIndoArabico);
+
+            Assert.AreEqual("D", numeralRomano);
+        }
+
+        [TestMethod]
+        public void Deve_Converter_1000_Para_M()
+        {
+            var numeroIndoArabico = 1000;
+
+            string numeralRomano = ConversorNumeraisRomanos.Converter(numeroIndoArabico);
+
+            Assert.AreEqual("M", numeralRomano);
         }
     }
 }
